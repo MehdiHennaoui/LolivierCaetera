276a0f497a3727bdcf7e6b0be482e1ed4723e1d8
MehdiHennaoui <medh31@gmail.com>
2016-07-28 15:52:21 +0200

    Creation de la vue acceuil -creation de la route accueil

---
+++ resources/views/accueil.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>L'olivier & Caetera</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+</head>
+<body>
+<h1>Hello</h1>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>	
+</body>
+</html>
