97a86b83af7204c6e8d2d0aca738b5556e111fce
MehdiHennaoui <medh31@gmail.com>
2016-08-27 17:16:50 +0200

    Creation model concert et migration de celui ci

---
+++ app/Concert.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Concert extends Model
+{
+    protected $fillable = ['date', 'hour', 'address', 'city', 'place'];
+}
--- database/migrations/2016_08_27_145005_create_concerts_table.php
+++ database/migrations/2016_08_27_145005_create_concerts_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateArticles extends Migration
+class CreateConcertsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,11 +12,13 @@ class CreateArticles extends Migration
      */
     public function up()
     {
-        Schema::create('articles', function (Blueprint $table) {
+        Schema::create('concerts', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title', 1000);
-            $table->string('subtitle', 1000);
-            $table->text('body');
+            $table->date('date');
+            $table->time('hour')->nullable();
+            $table->text('adress');
+            $table->string('city');
+            $table->string('place');
             $table->timestamps();
         });
     }
@@ -28,6 +30,6 @@ class CreateArticles extends Migration
      */
     public function down()
     {
-        Schema::drop('articles');
+        Schema::drop('concerts');
     }
 }
